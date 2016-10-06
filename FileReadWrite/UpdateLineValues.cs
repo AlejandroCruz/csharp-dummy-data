9fde11303f95c891e849da3e2cf9eb705eda8570
AlejandroCruz <acdeveloper@outlook.com>
2016-10-06 00:21:08 -0400

    Update

--- FileReadWrite/UpdateLineValues.cs
+++ FileReadWrite/UpdateLineValues.cs
@@ -23,7 +23,7 @@ namespace FileReadWrite
             objUpdate.TabularRow[1][index] = newString;
 
             System.Console.WriteLine("\nValue of 'oldString' is {0}", oldString);
-            System.Console.WriteLine("\nUpdated value of {0} is {1}", objUpdate.TabularRow[0][index], objUpdate.TabularRow[1][index]);
+            System.Console.WriteLine("\nUpdated value of '{0}' is {1}", objUpdate.TabularRow[0][index], objUpdate.TabularRow[1][index]);
         }
     }
 }
