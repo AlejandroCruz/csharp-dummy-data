148f27d12077924e772065d30008291960d703fb
AlejandroCruz <acdeveloper@outlook.com>
2016-11-27 22:13:51 -0400

    Basic logic for char incrementation to 4 elements set

--- FileReadWrite/RegexHandlerUtils.cs
+++ FileReadWrite/RegexHandlerUtils.cs
@@ -18,25 +18,117 @@ namespace FileReadWrite
                 prefixSequence.Add(item);
             }
 
-            for (int i = 0; i < prefixSequence.Count; i++)
+            /*
+             * English alphabet 26 letters:
+             *  26 ^ 4 = 456,976
+             *  26 ^ 3 = 17,576
+             *  26 ^ 2 = 676
+             */
+            int amountOfElements = prefixSequence.Count;
+            switch (amountOfElements)
             {
-                if (prefixSequence[i].Equals(validateZ))
-                {
-                    if (i == 4)
+                case 1:
+                    if (prefixSequence[0].Equals(validateZ))
                     {
-                        throw new Exception();
+                        prefixSequence[0] = 'A';
+                        prefixSequence.Add('A');
                     }
                     else
                     {
-                        prefixSequence[i] = 'A';
-                        prefixSequence.Add('A');
+                        prefixSequence[0]++;
                     }
-                }
-                else
-                {
-                    prefixSequence[i]++;
-                    outPrefix = prefixSequence[i].ToString();
-                }
+                    break;
+                case 2:
+                    if (prefixSequence[1].Equals(validateZ))
+                    {
+                        if (prefixSequence[0].Equals(validateZ))
+                        {
+                            prefixSequence[0] = 'A';
+                            prefixSequence[1] = 'A';
+                            prefixSequence.Add('A');
+                        }
+                        else
+                        {
+                            prefixSequence[0]++;
+                            prefixSequence[1] = 'A';
+                        }
+                    }
+                    else
+                    {
+                        prefixSequence[1]++;
+                    }
+                    break;
+                case 3:
+                    if (prefixSequence[2].Equals(validateZ))
+                    {
+                        if (prefixSequence[1].Equals(validateZ))
+                        {
+                            if (prefixSequence[0].Equals(validateZ))
+                            {
+                                prefixSequence[0] = 'A';
+                                prefixSequence[1] = 'A';
+                                prefixSequence[2] = 'A';
+                                prefixSequence.Add('A');
+
+                            }
+                            else
+                            {
+                                prefixSequence[0]++;
+                                prefixSequence[1] = 'A';
+                                prefixSequence[2] = 'A';
+                            }
+                        }
+                        else
+                        {
+                            prefixSequence[1]++;
+                            prefixSequence[2] = 'A';
+                        }
+                    }
+                    else
+                    {
+                        prefixSequence[2]++;
+                    }
+                    break;
+                case 4:
+                    throw new Exception();
+                //if (prefixSequence[3].Equals(validateZ))
+                //{
+                //    if (prefixSequence[2].Equals(validateZ))
+                //    {
+                //        if (prefixSequence[1].Equals(validateZ))
+                //        {
+                //            if (prefixSequence[0].Equals(validateZ))
+                //            {
+                //                throw new Exception();
+                //            }
+                //            else
+                //            {
+                //                prefixSequence[0]++;
+                //                prefixSequence[1] = 'A';
+                //                prefixSequence[2] = 'A';
+                //                prefixSequence[3] = 'A';
+                //            }
+                //        }
+                //        else
+                //        {
+                //            prefixSequence[1]++;
+                //            prefixSequence[2] = 'A';
+                //            prefixSequence[3] = 'A';
+                //        }
+                //    }
+                //    else
+                //    {
+                //        prefixSequence[2]++;
+                //        prefixSequence[3] = 'A';
+                //    }
+                //}
+                //else
+                //{
+                //    prefixSequence[3]++;
+                //}
+                //break;
+                default:
+                    break;
             }
 
             StringBuilder strBuild = new StringBuilder();
@@ -49,23 +141,3 @@ namespace FileReadWrite
         }
     }
 }
-//if (prefixSequence.Count > 1)
-//            {
-//                for (int i = 0; i<prefixSequence.Count; i++)
-//                {
-//                    StringBuilder strBuild = new StringBuilder();
-
-//                    if (prefixSequence[i + 1].Equals(validateZ))
-//                    {
-//                        prefixSequence[i + 1] = 'A';
-//                        char test = prefixSequence[i + 2]++;
-//                    }
-
-//                    foreach (var item in prefixSequence)
-//                    {
-//                        strBuild.Append(item);
-//                    }
-
-//                    outPrefix = strBuild.ToString();
-//                }
-//            }
